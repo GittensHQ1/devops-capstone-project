7b7d584d784eae0c33d4ae41f611921216f89717
GittensHQ1 <158979638+GittensHQ1@users.noreply.github.com>
2024-02-07 21:35:23 -0500

    Update routes.py

--- service/routes.py
+++ service/routes.py
@@ -61,8 +61,8 @@ def create_accounts():
 # LIST ALL ACCOUNTS
 ######################################################################
 
-
 @app.route("/accounts", methods=["GET"])
+
 def list_accounts():
     """
     List all Accounts
@@ -82,6 +82,7 @@ def list_accounts():
 
 
 @app.route("/accounts/<int:account_id>", methods=["GET"])
+
 def get_accounts(account_id):
     """
     Reads an Account
@@ -101,6 +102,7 @@ def get_accounts(account_id):
 ######################################################################
 
 @app.route("/accounts/<int:account_id>", methods=["PUT"])
+
 def update_accounts(account_id):
     """
     Update an Account
@@ -123,6 +125,7 @@ def update_accounts(account_id):
 ######################################################################
 
 @app.route("/accounts/<int:account_id>", methods=["DELETE"])
+
 def delete_accounts(account_id):
     """
     Delete an Account
